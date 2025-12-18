e0720c8e3e01fb7774350efddc469febe32ac6f8
nathanwn <49882031+nathanwn@users.noreply.github.com>
2025-12-18 21:46:53 +0700

    Replace deprecated API StdFileTypes.JAVA

--- src/main/java/net/egork/chelper/ui/ClassSelector.java
+++ src/main/java/net/egork/chelper/ui/ClassSelector.java
@@ -1,6 +1,6 @@
 package net.egork.chelper.ui;
 
-import com.intellij.openapi.fileTypes.StdFileTypes;
+import com.intellij.ide.highlighter.JavaFileType;
 import com.intellij.openapi.project.Project;
 import com.intellij.ui.EditorTextField;
 import com.intellij.ui.JavaReferenceEditorUtil;
@@ -10,6 +10,6 @@ import com.intellij.ui.JavaReferenceEditorUtil;
  */
 public class ClassSelector extends EditorTextField {
     public ClassSelector(String text, Project project) {
-        super(JavaReferenceEditorUtil.createDocument(text, project, true), project, StdFileTypes.JAVA);
+        super(JavaReferenceEditorUtil.createDocument(text, project, true), project, JavaFileType.INSTANCE);
     }
 }
